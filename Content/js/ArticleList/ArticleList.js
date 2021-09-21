fde78a6b1f8f83f4ae692cee9c02542456e6959e
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2021-09-21 12:30:37 +0800

    feat：文章圖片新增地名

--- Content/js/ArticleList/ArticleList.js
+++ Content/js/ArticleList/ArticleList.js
@@ -5,6 +5,7 @@ const Article = {
     //     category : "front_end",
     //     create_time : "2021/08/14",
     //     img_route : "/Content/img/Article/2021/08/Demo/Demo.jpg",
+    //     img_place_name : "none",//圖片地名，none不顯示
     //     article_route:"/Article/2021/08/Demo.html",
     // },
     "CSS_Specificity" : {
@@ -13,6 +14,7 @@ const Article = {
         category : "front_end",
         create_time : "2021/09/19",
         img_route : "/Content/img/Article/2021/09/CSS_Specificity/CSS_Specificity.jpg",
+        img_place_name : "花蓮",
         article_route:"/Article/2021/09/CSS_Specificity.html",
     },
 };
