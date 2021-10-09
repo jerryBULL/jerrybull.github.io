4af1d4c79c1537ed45db9c643e5a64ebe5675ecf
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2021-10-09 11:39:56 +0800

    feat：文章++ || 調整文章內圖片不要有連結

--- Content/js/ArticleList/ArticleList.js
+++ Content/js/ArticleList/ArticleList.js
@@ -8,6 +8,15 @@ const Article = {
     //     img_place_name : "none",//圖片地名，none不顯示
     //     article_route:"/Article/2021/08/Demo.html",
     // },
+    "PHP_inculd&require" : {
+        title : "PHP-inculd&require差別",
+        tag : ["php"],
+        category : "rear_end",
+        create_time : "2021/10/09",
+        img_route : "/Content/img/Article/2021/10/PHP_inculd&require/PHP_inculd&require.jpg",
+        img_place_name : "花蓮清水斷崖",
+        article_route:"/Article/2021/10/PHP_inculd&require.html",
+    },
     "CSS_Specificity" : {
         title : "CSS權重(CSS Specificity)",
         tag : ["css"],
